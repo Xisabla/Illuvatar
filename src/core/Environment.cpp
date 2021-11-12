8c291050eb50e027d88899c15949c5edcc20f6d6
Thiebosh <thibaut.juzeau@isen.yncrea.fr>
2021-11-12 22:46:40 +0100

    dynamic access to json

--- src/core/Environment.cpp
+++ src/core/Environment.cpp
@@ -9,7 +9,13 @@
 =========================================================================*/
 #include "core/Environment.h"
 
+#include <iostream>
 #include <fstream>
+#include <filesystem>
+#include <string>
+#include <algorithm>
+
+namespace fs = std::filesystem;
 
 Environment* Environment::_instance= nullptr;
 
@@ -20,8 +26,21 @@ Environment::Environment(std::string filePath) {
 }
 
 Environment* Environment::instance() {
-    if(Environment::_instance==nullptr){
-        Environment::_instance = new Environment("environment.json");//"../build/environment.json"
+    if(Environment::_instance==nullptr) {
+        std::string projectName = "Illuvatar";
+
+        std::string path_string = fs::current_path().string();
+        auto pos = path_string.find(projectName);
+        if (pos == std::string::npos) {
+            std::cout << "Game folder not found!" << std::endl;
+            exit(1);
+        }
+        std::string sub = path_string.substr(pos+projectName.size());
+        if (sub[sub.size()-1] != '/') path_string += "/";
+        for (int i = 0; i < std::count(sub.begin(), sub.end(), '/'); ++i) path_string += "../";
+        path_string += "build/environment.json";
+
+        Environment::_instance = new Environment(path_string);
     }
     return _instance;
 }
